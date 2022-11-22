acfe409e36661d67ce4ff27c5159282a895bcdf8
duckienmai601 <duckienmai601@gmail.com>
2022-11-22 13:57:37 +0700

    first commit

---
+++ Facility/style.js
@@ -0,0 +1,5 @@
+const searchBtn=document.querySelector('search-btn')
+let searchInput=document.getElementById('.search-input')
+searchBtn.addEventListener('click',function(){
+	window.location.href='/SanPham/category/' + (searchInput.innerText)+'.html'	
+})
