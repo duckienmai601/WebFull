95e95e2110cca966658b5dea73046b7fcb98eac5
duckienmai601 <duckienmai601@gmail.com>
2022-11-26 16:55:44 +0700

    update

--- Facility/style.js
+++ Facility/style.js
@@ -1,6 +1,21 @@
-const searchBtn=document.querySelector('search-btn')
-let searchInput=document.getElementById('.search-input')
-searchBtn.addEventListener('click',function(){
-	window.location.href='/SanPham/category/' + (searchInput.innerText)+'.html'	
-})
+
  
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+	document.getElementById('myUL').style.display = "block";
+
+}
